ae72fcff069724786c522fc170f462db97b6be3f
Ashmita-03 <ashmitajyothilingam@gmail.com>
2025-09-13 19:05:55 +0000

    init

--- README.md
+++ README.md
@@ -21,8 +21,11 @@ You can extend your project further by:
 
 ###  Attributions
 *If you used any code, ideas, images, or resources from another person or group of people, tell us about it here. Make sure it is in the public domain, has a license that allows you to use it, or is one of your own.
-- Add here if applicable
+- grass: https://pixabay.com/photos/grass-trees-forest-meadow-field-315184/
+  
+- dustbin: [https://www.google.com/search?q=cartoon+picnic+basket+transparent&sca_esv=4bca1a4895ed980b&rlz=1C1VDKB_enUS1148US1150&udm=2&biw=1280&bih=551&sxsrf=AE3TifNVgzJBYczd5ypIuK2jaqUcbjBAog%3A1752208649844&ei=CZVwaOGhM7DV5NoPsuyz8Ak&ved=0ahUKEwihypXx_bOOAxWwKlkFHTL2DJ4Q4dUDCBI&uact=5&oq=cartoon+picnic+basket+transparent&gs_lp=EgNpbWciIWNhcnRvb24gcGljbmljIGJhc2tldCB0cmFuc3BhcmVudDIEEAAYHki7DlC8AliKDnABeACQAQCYAVWgAagFqgECMTG4AQPIAQD4AQGYAgygAswFwgIKEAAYgAQYQxiKBcICBhAAGAcYHsICBRAAGIAEwgIGEAAYCBgewgIGEAAYBRgemAMAiAYBkgcCMTKgB_oNsgcCMTG4B8QFwgcGMC4xMC4yyAcd&sclient=img#vhid=cjJauXRpsurvCM&vssid=mosaic](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdJvtWUuvaBCF1vEKngtJKO9Tv5tYV8SN8fQ&s)
 
+- trashbag: https://image.similarpng.com/file/similarpng/very-thumbnail/2021/05/Big-black-plastic-of-trash-bag-on-transparent-background-PNG.png
 ---
 
 ## File Overview
--- script.js
+++ script.js
@@ -1,35 +1,94 @@
-//Move the catcher with the left and right arrow keys to catch the falling objects. 
+//Move the catcher with the left and right arrow keys to catch the falling apples.
 
 /* VARIABLES */
 let catcher, fallingObject;
+let score = 0;
+let backgroundImg;
+let catcherImg; 
+let fallingObjectImg;
+//let fallingObjectText;
+
 
 /* PRELOAD LOADS FILES */
 function preload(){
-  
+  backgroundImg = loadImage("assets/grass.jpg");
+  catcherImg = loadImage("assets/dustbin.jpeg");
+  fallingObjectImg = loadImage("assets/trash1.png"); 
 }
-
 /* SETUP RUNS ONCE */
 function setup() {
-  createCanvas(400,400);
-  background(224,224,224);
+  createCanvas(400,410);
+  
+backgroundImg.resize(400,410);
+catcherImg.resize(100,0);
+fallingObjectImg.resize(29,0);
   
   //Create catcher 
-  catcher = new Sprite(200,380,100,20);
+  //catcher = new Sprite(catcherImg, 200,380,"k");
+  catcher = new Sprite(catcherImg, 200,380,100,20,"k");
   catcher.color = color(95,158,160);
-  
+ 
   //Create falling object
-  fallingObject = new Sprite(100,0,10);
+  fallingObject = new Sprite(fallingObjectImg,100,0,10);
   fallingObject.color = color(0,128,128);
-
+  fallingObject.vel.y = 2;
+  fallingObject.rotationLock = true;
+  //fallingObject.text = random(fallingObjectText);
 }
 
 /* DRAW LOOP REPEATS */
 function draw() {
+  background(224,224,224);
+
+  // Draw background image
+image(backgroundImg,0,0);
   
   // Draw directions to screen
-  fill(0);
+  fill("white");
   textSize(12);
   text("Move the \ncatcher with the \nleft and right \narrow keys to \ncatch the falling \nobjects.", width-100, 20);
 
+  //if fallingObject reaches bottom, move back to random position at top
+  if(fallingObject.y >= height) {
+    fallingObject.y = 0;
+    fallingObject.x = random(width);
+    fallingObject.vel.y = random(1,5);
+    
+    //Mild
+    score = score - 1;
+}
+
   
+  //Move catcher
+  if (kb.pressing("left")) {
+    catcher.vel.x = -4.5;
+  } else if (kb.pressing("right")) {
+    catcher.vel.x = 4.5;
+  } else {
+    catcher.vel.x = 0;
+  }
+
+  //Stop catcher at egdes of screen
+  if (catcher.x < 50) {
+    catcher.x = 50;
+  } else if (catcher.x > 350) {
+    catcher.x = 350;
+  }
+
+// If fallingObject collides with catcher, move back to random position at top
+  if (fallingObject.collides(catcher)) {
+    fallingObject.y = 0;
+    fallingObject.x = random(width);
+    fallingObject.vel.y = random(1,5);
+    fallingObject.direction = "down";
+    //fallingObject.text = random(fallingObjectText);
+    score = score + 1
+  }
+
+  // Draw the score to screen
+  fill(0,128,128);
+  textSize(20);
+  text("Score = " + score,10,30);
+
+  //allSprites.debug = mouse.pressing();
 }
